1e3553cfe3942dee326d388c0c4b1550b8d7c300
acharl <amadeo.charle108@gmail.com>
2021-12-06 14:13:16 +0100

    chore(): DEV

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,72 +1,72 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-// use substrate_subxt::{Client, PairSigner};
-// use substrate_subxt::{ClientBuilder, Error, NodeTemplateRuntime};
-// use crypto::{
-//     helper::Helper,
-//     proofs::{keygen::KeyGenerationProof},
-//     random::Random};
-// use hex_literal::hex;
-// use pallet_mixnet::types::{PublicKeyShare};
-// use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
-// mod substrate;
-// use substrate::rpc::store_public_key_share;
+use substrate_subxt::{Client, PairSigner};
+use substrate_subxt::{ClientBuilder, Error, NodeTemplateRuntime};
+use crypto::{
+    helper::Helper,
+    proofs::{keygen::KeyGenerationProof},
+    random::Random};
+use hex_literal::hex;
+use pallet_mixnet::types::{PublicKeyShare};
+use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
+mod substrate;
+use substrate::rpc::store_public_key_share;
 
 
-// fn get_sealer(sealer: String) -> (Pair, [u8; 32]) {
-//     // get the sealer and sealer_id
-//     if sealer == "bob" {
-//         return (
-//             AccountKeyring::Bob.pair(),
-//             hex!("8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48").into(),
-//         );
-//     } else {
-//         return (
-//             AccountKeyring::Charlie.pair(),
-//             hex!("90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22").into(),
-//         );
-//     };
-// }
+fn get_sealer(sealer: String) -> (Pair, [u8; 32]) {
+    // get the sealer and sealer_id
+    if sealer == "bob" {
+        return (
+            AccountKeyring::Bob.pair(),
+            hex!("8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48").into(),
+        );
+    } else {
+        return (
+            AccountKeyring::Charlie.pair(),
+            hex!("90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22").into(),
+        );
+    };
+}
 
-// async fn init() -> Result<Client<NodeTemplateRuntime>, Error> {
-//     let url = "ws://127.0.0.1:9944";
-//     let client = ClientBuilder::<NodeTemplateRuntime>::new()
-//         .set_url(url)
-//         .build()
-//         .await?;
-//     Ok(client)
-// }
+async fn init() -> Result<Client<NodeTemplateRuntime>, Error> {
+    let url = "ws://127.0.0.1:9944";
+    let client = ClientBuilder::<NodeTemplateRuntime>::new()
+        .set_url(url)
+        .build()
+        .await?;
+    Ok(client)
+}
 
 #[get("/keygen")]
 async fn hello() -> impl Responder {
 
-    // let vote = "TEST_VOTE";
-    // let sk_as_string = "10008";
-    // let sealer = "bob";
-    // let client = init().await.unwrap();
+    let vote = "TEST_VOTE";
+    let sk_as_string = "10008";
+    let sealer = "bob";
+    let client = init().await.unwrap();
 
-    //  // create private and public key
-    //  let (params, sk, pk) = Helper::setup_lg_system_with_sk(sk_as_string.as_bytes());
+     // create private and public key
+     let (params, sk, pk) = Helper::setup_lg_system_with_sk(sk_as_string.as_bytes());
 
-    //  // get the sealer and sealer_id
-    //  let (sealer, sealer_id): (Pair, [u8; 32]) = get_sealer(sealer.to_string()); // changed to &
+     // get the sealer and sealer_id
+     let (sealer, sealer_id): (Pair, [u8; 32]) = get_sealer(sealer.to_string()); // changed to &
  
-    //  // create public key share + proof
-    //  let r = Random::get_random_less_than(&params.q());
-    //  let proof = KeyGenerationProof::generate(&params, &sk.x, &pk.h, &r, &sealer_id);
-    //  let pk_share = PublicKeyShare {
-    //      proof: proof.clone().into(),
-    //      pk: pk.h.to_bytes_be(),
-    //  };
-    //  let vote_id = vote.as_bytes().to_vec();
+     // create public key share + proof
+     let r = Random::get_random_less_than(&params.q());
+     let proof = KeyGenerationProof::generate(&params, &sk.x, &pk.h, &r, &sealer_id);
+     let pk_share = PublicKeyShare {
+         proof: proof.clone().into(),
+         pk: pk.h.to_bytes_be(),
+     };
+     let vote_id = vote.as_bytes().to_vec();
  
-    //  // submit the public key share + proof
-    //  let signer = PairSigner::<NodeTemplateRuntime, Pair>::new(sealer);
-    //  let store_public_key_share_response =
-    //      store_public_key_share(&client, &signer, vote_id, pk_share).await.unwrap();
-    //  println!(
-    //      "store_public_key_share_response: {:?}",
-    //      store_public_key_share_response.events[0].variant
-    //  );
+     // submit the public key share + proof
+     let signer = PairSigner::<NodeTemplateRuntime, Pair>::new(sealer);
+     let store_public_key_share_response =
+         store_public_key_share(&client, &signer, vote_id, pk_share).await.unwrap();
+     println!(
+         "store_public_key_share_response: {:?}",
+         store_public_key_share_response.events[0].variant
+     );
  
     HttpResponse::Ok().body("Hello world!")
 
--- randomizer/src/main.rs
+++ randomizer/src/main.rs
@@ -2,20 +2,20 @@ mod health;
 mod index;
 mod randomizer;
 
-use actix_web::{App, HttpServer};
-use health::get_health;
-use index::get_index;
-use randomizer::randomize_ballot;
+// use actix_web::{App, HttpServer};
+// use health::get_health;
+// use index::get_index;
+// use randomizer::randomize_ballot;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(get_index)
-            .service(get_health)
-            .service(randomize_ballot)
-    })
-    .bind(("0.0.0.0", 8081))?
-    .run()
-    .await
-}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(get_index)
+//             .service(get_health)
+//             .service(randomize_ballot)
+//     })
+//     .bind(("0.0.0.0", 8081))?
+//     .run()
+//     .await
+// }
