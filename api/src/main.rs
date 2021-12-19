77f4c8de49b63662a1f1f5dc2c902ec9346c72fb
acharl <amadeo.charle108@gmail.com>
2021-12-19 18:14:30 +0100

    feat(decrypt): convert HexDecryptionProof into DecryptedShareProof

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,14 +2,15 @@ use actix_web::{web,get,  post, App, HttpResponse, HttpServer, Responder, Result
 use substrate_subxt::{Client, PairSigner};
 use substrate_subxt::{ClientBuilder, Error, NodeTemplateRuntime};
 use hex_literal::hex;
-use pallet_mixnet::types::{Cipher, PublicKeyShare, Wrapper};
+use pallet_mixnet::types::{Cipher, PublicKeyShare, DecryptedShareProof, Wrapper};
 use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
 mod substrate;
 use substrate::rpc::{get_ciphers, store_public_key_share, submit_partial_decryptions};
 use crypto::{
-    proofs::decryption::{DecryptPostBody},
+    proofs::decryption::{DecryptPostBody, HexDecryptionProof},
     types::Cipher as BigCipher,
 };
+
 
 fn get_sealer(sealer: String) -> (Pair, [u8; 32]) {
     // get the sealer and sealer_id
@@ -90,7 +91,35 @@ async fn post_decrypt(web::Path((vote, question, sealer)): web::Path<(String, St
     let vote_id = vote.as_bytes().to_vec();
     let topic_id = question.as_bytes().to_vec();
     let nr_of_shuffles = 3;
-  
+
+
+    // This is what a DecryptionProof proof looks like when
+    // we log it as 
+    // 
+    // println!("CHALLENGE: {:?}", proof.challenge.to_str_radix(16));
+    // println!("RESPONSE: {:?}", proof.response.to_str_radix(16));
+    // 
+    // DecryptionProof {
+    //     CHALLENGE: "5f8d6b6156655e054edbc45c4748152d621d3e965ac734db12ed0dd89a35cf4c92905a1e0c3d08c794d4640f1139f6cb0d8d9f3823cd78ca159a0072c836cf12"
+    //     RESPONSE: "73f9c243fc22ed0c47024839b0bc66c0a9514ff50f5d95804dce0653df2b3a8c551c9cb417a2136c60e2c421b172ea74139af3bbb11ffa38e34fe1f0c3be5d1b8f6329a1ddfdd4bd6d585514cae5ac9429508622fbf5008c6ef2497b1126a86cb1ffcdd59d8fc0d4e562e466af1470e88e708c580fe4aa9112f2848780bd55bf7c92cc7a684bcced3be0e729c558103af50b74f6e29cbdf186666bf8af3b1e2ee84a92038c602b8c3eda835c158d1fb1df04d4ab7e9ce77b7fe010aa91c400c94a8b5c8af4876893561ecbc02d9c7eafd9efc964ad898a74c73cd3c53edfce94d5164c34bf07409c1494a5158123052de607ce93f2baf64f395aa1b9b26f00d"    
+    // }
+    // 
+    // Perhaps it would be ideal to pass the DecryptionProof with challenge and 
+    // response encoded as hex strings as above. 
+    // We could then simply convert it using from_str_radix()
+
+
+    
+    let raw_decryption_proof: HexDecryptionProof = decrypt_post_body.decryption_proof.clone().into();
+
+
+    
+    let decryption_proof = DecryptedShareProof {
+        challenge: raw_decryption_proof.challenge.into_bytes(),
+        response: raw_decryption_proof.response.into_bytes()
+    };
+
+
     let signer = PairSigner::<NodeTemplateRuntime, Pair>::new(sealer);
     let response = submit_partial_decryptions(
         &client,
@@ -98,7 +127,7 @@ async fn post_decrypt(web::Path((vote, question, sealer)): web::Path<(String, St
         vote_id,
         topic_id,
         decrypt_post_body.shares.clone(),
-        decrypt_post_body.decryption_proof.clone().into(),
+        decryption_proof,
         nr_of_shuffles,
     )
     .await.unwrap();
@@ -112,6 +141,7 @@ async fn main() -> std::io::Result<()> {
         .service(keygen)
         .service(get_decrypt)
         .service(post_decrypt)
+        .data(web::JsonConfig::default().limit(1024 * 1024 * 50))
     })
     .bind(("0.0.0.0", 11111))?
     .run()
